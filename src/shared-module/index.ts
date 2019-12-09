faa0f9932a309fe00a4afc6f3c3a2ba819d21636
astorozhevsky <astorozhevsky@ronasit.com>
2019-12-09 19:19:49 +0600

    refactoring

--- src/shared-module/index.ts
+++ src/shared-module/index.ts
@@ -3,7 +3,7 @@ import {
   getProjectPath,
   getRoutingModulePath,
   parseLocation
-} from '../../core';
+  } from '../../core';
 import {
   apply,
   chain,
@@ -15,52 +15,68 @@ import {
   template,
   Tree,
   url
-} from '@angular-devkit/schematics';
+  } from '@angular-devkit/schematics';
+import { join, Path, strings } from '@angular-devkit/core';
 import { Schema as SharedModuleOptions } from './schema';
-import { strings, join, Path } from '@angular-devkit/core';
+
+function prepareOptions(host: Tree, options: SharedModuleOptions): void {
+  prepareOptionsType(options);
+  prepareOptionsPath(host, options);
+}
+
+function prepareOptionsType(options: SharedModuleOptions): void {
+  if (!options.type) {
+    if (options.component) {
+      options.type = 'component';
+    } else if (options.directive) {
+      options.type = 'directive';
+    } else if (options.pipe) {
+      options.type = 'pipe';
+    } else if (options.service) {
+      options.type = 'service';
+    } else {
+      options.type = 'component';
+    }
+  }
+}
+
+function prepareOptionsPath(host: Tree, options: SharedModuleOptions): void {
+  if (!options.path) {
+    options.path = getProjectPath(host, options);
+
+    if (options.section) {
+      options.path = join(options.path as Path, options.section);
+    }
+
+    options.path = join(options.path as Path, 'shared', options.name);
+  }
+
+  const location = parseLocation(options.path, options.name);
+
+  options.name = location.name;
+  options.path = location.path;
+}
+
+function getTemplatesPath(options: SharedModuleOptions): string {
+  let templatesPath = './files';
+  if (options.type === 'component') {
+    templatesPath = `${templatesPath}/component`;
+  } else if (options.type === 'directive') {
+    templatesPath = `${templatesPath}/directive`;
+  } else if (options.type === 'pipe') {
+    templatesPath = `${templatesPath}/pipe`;
+  } else if (options.type === 'service') {
+    templatesPath = `${templatesPath}/service`;
+  }
+
+  return templatesPath;
+}
 
 export default function (options: SharedModuleOptions): Rule {
   return (host: Tree, context: SchematicContext) => {
-    if (!options.type) {
-      if (options.component) {
-        options.type = 'component';
-      } else if (options.directive) {
-        options.type = 'directive';
-      } else if (options.pipe) {
-        options.type = 'pipe';
-      } else if (options.service) {
-        options.type = 'service';
-      } else {
-        options.type = 'component';
-      }
-    }
+    prepareOptions(host, options);
 
-    if (!options.path) {
-      options.path = getProjectPath(host, options);
-
-      if (options.section) {
-        options.path = join(options.path as Path, options.section);
-      }
-
-      options.path = join(options.path as Path, 'shared', options.name);
-    }
-
-    const location = parseLocation(options.path, options.name);
-
-    options.name = location.name;
-    options.path = location.path;
-
-    let templatesPath = './files';
-    if (options.type === 'component') {
-      templatesPath = `${templatesPath}/component`;
-    } else if (options.type === 'directive') {
-      templatesPath = `${templatesPath}/directive`;
-    } else if (options.type === 'pipe') {
-      templatesPath = `${templatesPath}/pipe`;
-    } else if (options.type === 'service') {
-      templatesPath = `${templatesPath}/service`;
-    }
-
+    const templatesPath = getTemplatesPath(options);
     const templateSource = apply(url(templatesPath), [
       template({
         ...options,
