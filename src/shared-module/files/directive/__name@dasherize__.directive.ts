4083b83d725af878d7cda0f9bf145f29123dc9ff
astorozhevsky <astorozhevsky@ronasit.com>
2019-12-10 17:31:44 +0600

    fixed directive + empty line

--- src/shared-module/files/directive/__name@dasherize__.directive.ts
+++ src/shared-module/files/directive/__name@dasherize__.directive.ts
@@ -1,7 +1,7 @@
 import { Directive, ElementRef } from '@angular/core';
 
 @Directive({
-  selector: '[<%= dasherize(((hasSection && !hasPage) ? (section + ' '): '') + ((hasSection && hasPage) ? (page + ' ') : '') + name) %>]'
+  selector: '[<%= camelize(((hasSection && !hasPage) ? (section + ' '): '') + ((hasSection && hasPage) ? (page + ' ') : '') + name) %>]'
 })
 export class <%= classify(((hasSection) ? (section + ' '): '') + ((hasSection && hasPage) ? (page + ' ') : '') + name) %>Directive {
   constructor(
