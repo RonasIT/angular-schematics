3a46de16513a9f59db326e5b8efd79b69291d53e
Vitaly Iskrin <viskrin@ronasit.com>
2020-12-03 21:56:55 +0600

    #-: Code style fixes

--- src/facade/files/__name@dasherize__/__name@dasherize__.facade.ts
+++ src/facade/files/__name@dasherize__/__name@dasherize__.facade.ts
@@ -1,13 +1,13 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';<% if (store) { %>
-import { <%= classify(section + ' ' + ((hasParent) ? (parent + ' ') : '') + name) %>PageSelectors } from './shared/store';<% } %><% if (isNgrxInstalled) { %>
+import { <%= classify(section, parent, name) %>PageSelectors } from './shared/store';<% } %><% if (isNgrxInstalled) { %>
 import { AppState } from '@shared/store';<% } %>
 
 @Injectable()
-export class <%= classify(section + ' ' + ((hasParent) ? (parent + ' ') : '') + name) %>PageFacade {
+export class <%= classify(section, parent, name) %>PageFacade {
   <% if (store) { %>public get isLoading$(): Observable<boolean> {
-    return this.store.select(<%= classify(section + ' ' + ((hasParent) ? (parent + ' ') : '') + name) %>PageSelectors.isLoading);
+    return this.store.select(<%= classify(section, parent, name) %>PageSelectors.isLoading);
   }<% } %>
 <% if (isNgrxInstalled) { %>
   constructor(
--- src/page-module/files/__name@dasherize__/__name@dasherize__.module.ts
+++ src/page-module/files/__name@dasherize__/__name@dasherize__.module.ts
@@ -1,20 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { <%= classify(section + ' ' + ((hasParent) ? (parent + ' ') : '') + name) %>PageComponent } from './<%= dasherize(name) %>.component';
-import { <%= classify(section + ' ' + ((hasParent) ? (parent + ' ') : '') + name) %>PageRoutingModule } from './<%= dasherize(name) %>.routing';
+import { <%= classify(section, parent, name) %>PageComponent } from './<%= dasherize(name) %>.component';
+import { <%= classify(section, parent, name) %>PageRoutingModule } from './<%= dasherize(name) %>.routing';
 import { RouterModule } from '@angular/router';<% if (isNgxTranslateInstalled) { %>
 import { TranslateModule } from '@ngx-translate/core';<% } %>
 
 @NgModule({
   declarations: [
-    <%= classify(section + ' ' + ((hasParent) ? (parent + ' ') : '') + name) %>PageComponent
+    <%= classify(section, parent, name) %>PageComponent
   ],
   imports: [
     CommonModule,
     RouterModule,<% if (isNgxTranslateInstalled) { %>
     TranslateModule,<% } %>
-    <%= classify(section + ' ' + ((hasParent) ? (parent + ' ') : '') + name) %>PageRoutingModule
+    <%= classify(section, parent, name) %>PageRoutingModule
   ],
   providers: []
 })
-export class <%= classify(section + ' ' + ((hasParent) ? (parent + ' ') : '') + name) %>PageModule { }
+export class <%= classify(section, parent, name) %>PageModule { }
