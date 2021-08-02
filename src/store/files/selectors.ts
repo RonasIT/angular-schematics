b3ac883167e4e923cedc8517ba598266343587a9
astorozhevsky <astorozhevsky@ronasit.com>
2021-08-02 17:47:49 +0600

    updated dependencies + added eslint rules + fixed typing in selectors

--- src/store/files/selectors.ts
+++ src/store/files/selectors.ts
@@ -2,7 +2,7 @@ import { AppState } from '@shared/store';
 import { createSelector, MemoizedSelector } from '@ngrx/store';
 import { <%= classify(section, parent, page, (page) ? 'Page' : name) %>State } from './state';
 
-const selectFeature = (state: AppState) => state.<%= camelize(section, parent, page, (page) ? 'Page' : name) %> as <%= classify(section, parent, page, (page) ? 'Page' : name) %>State;
+const selectFeature = (state: AppState): <%= classify(section, parent, page, (page) ? 'Page' : name) %>State => state.<%= camelize(section, parent, page, (page) ? 'Page' : name) %> as <%= classify(section, parent, page, (page) ? 'Page' : name) %>State;
 
 export class <%= classify(section, parent, page, (page) ? 'Page' : name) %>Selectors {
   public static isLoading: MemoizedSelector<AppState, boolean> = createSelector(
